440e150af8df1bd87e8d3a9bdc1b52084db455dc
ivanneto02 <72827265+ivanneto02@users.noreply.github.com>
2021-06-22 18:06:29 -0700

    got rid of old files, added frameless window hint

--- open_main.py
+++ open_main.py
@@ -13,49 +13,57 @@ class Start(QMainWindow):
     This is the main window that appears when we start the application. This window
     serves no utility purpose and is only for the display of the program's logo.
     '''
-    def __init__(self, parent=None):
-        super(Start, self).__init__(parent)
+    def __init__(self):
+        super().__init__()
 
-        self.layout = QVBoxLayout()
+        self.w = None # No external window yet.
+
+        QTimer.singleShot(2000, self.show_next_instance)
+
         self.setWindow()
+        self.setWindowTitle("oof")
+
+    def show_next_instance(self):
+        if self.w is None:
+            self.w = start_main.ZenDocs()
+            self.close()
+        self.w.showMaximized()
 
     '''Sets the layout of the window'''
     def setWindow(self):
-        pass
+        title = "Zendocs"
+        icon = "img/icon2.png"
 
-'''Goes to the next iteration of the program - the ZenDocs file'''
-def show_next_instance():
-    nextw = start_main.ZenDocs()
-    nextw.show()
-    print('test10')
+        stylesheet = "background-image: url(./img/intrologo.jpg);" \
+                        "background-repeat: no-repeat;" \
+                        "background-position: center"
+
+        root = Tk()
+        height = root.winfo_screenheight()
+        width = root.winfo_screenwidth()
+
+        startTop, startLeft = height//4, width//4
+
+        self.setWindowTitle(title)
+        self.setWindowIcon(QIcon(icon))
+        self.setStyleSheet(stylesheet)
+
+        self.setGeometry(startLeft, startTop, width//2, height//2)
+
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
 def main():
     # Initialize application
     app = QApplication([])
 
-    print('test1')
-
     # Initialize widget
     mainWindow = Start()
-
-    print('test2')
 
     # Show the window
     mainWindow.show()
 
-    QTimer.singleShot(3000, app)
-    mainWindow.close()
-
-    print('test3')
-
-    # Call next instance of the program
-    show_next_instance()
-
-    print('test0')
-
-    # Execute the application
+    # Exit
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
 	main()
